34ca5bdc7d2192fa0e14e1cff99300567dc3b0cc
CindoddCindy <cindodcindy@gmail.com>
2021-06-20 13:29:25 +0700

    tambah layout

---
+++ vieroShoesAdminApp/app/src/main/java/com/cindodcindy/vieroshoesadmin/view/BottomNavAdmin.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.vieroshoesadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.vieroshoesadmin.R;
+
+public class BottomNavAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_admin);
+    }
+}
---
+++ vieroShoesAdminApp/app/src/main/java/com/cindodcindy/vieroshoesadmin/view/Login.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.vieroshoesadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.vieroshoesadmin.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
