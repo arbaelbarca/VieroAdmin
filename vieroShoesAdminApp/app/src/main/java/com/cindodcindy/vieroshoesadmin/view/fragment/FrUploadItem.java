f3026200936fdc05afdf07191e1f925405704bfb
CindoddCindy <cindodcindy@gmail.com>
2021-07-05 21:33:26 +0700

    tambahi permision

--- vieroShoesAdminApp/app/src/main/java/com/cindodcindy/vieroshoesadmin/view/fragment/FrUploadItem.java
+++ vieroShoesAdminApp/app/src/main/java/com/cindodcindy/vieroshoesadmin/view/fragment/FrUploadItem.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.cindodcindy.vieroshoesadmin.R;
+import com.cindodcindy.vieroshoesadmin.view.UploadImageActivity;
 import com.cindodcindy.vieroshoesadmin.view.adapter.AdapterChatAdmin;
 import com.cindodcindy.vieroshoesadmin.view.adapter.AdapterChatCustomer;
 import com.cindodcindy.vieroshoesadmin.view.model.ModelForItem;
@@ -72,178 +73,24 @@ public class FrUploadItem extends Fragment {
         }
     }
 
-    private ImageView imageView_upload_sepatu;
-    private EditText editText_nama, editText_ukuran, editText_harga, editText_lokasi;
-    private Button button_upload_image;
-
-    // Uri indicates, where the image will be picked from
-    private Uri filePath;
-
-    // request code
-    private final int PICK_IMAGE_REQUEST = 22;
-
-    // instance for firebase storage and StorageReference
-    FirebaseStorage storage;
-    StorageReference storageReference;
+    public Button button_add_stock;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
          View view = inflater.inflate(R.layout.fragment_fr_upload_item, container, false);
-
-         imageView_upload_sepatu=view.findViewById(R.id.iv_up_sepatu_image);
-         editText_nama=view.findViewById(R.id.et_up_nama_sepatu);
-         editText_ukuran=view.findViewById(R.id.et_up_uk_sepatu);
-         editText_harga=view.findViewById(R.id.et_up_harga_sepatu);
-         editText_lokasi=view.findViewById(R.id.et_up_lokasi_sepatu);
-         button_upload_image=view.findViewById(R.id.btn_up_btn_sepatu);
-
-         button_upload_image.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-
-             }
-         });
-
-         imageView_upload_sepatu.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-
-             }
-         });
+        button_add_stock=view.findViewById(R.id.btn_add_stock);
+        button_add_stock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UploadImageActivity.class);
+                startActivity(intent);
+            }
+        });
 
               return view;
     }
 
-    // Select Image method
-    private void SelectImage()
-    {
 
-        // Defining Implicit Intent to mobile gallery
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(
-                        intent,
-                        "Select Image from here..."),
-                PICK_IMAGE_REQUEST);
-    }
-
-    // Override onActivityResult method
-    @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode,
-                                    Intent data)
-    {
-
-        super.onActivityResult(requestCode,
-                resultCode,
-                data);
-
-        // checking request code and result code
-        // if request code is PICK_IMAGE_REQUEST and
-        // resultCode is RESULT_OK
-        // then set image in the image view
-        if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null
-                && data.getData() != null) {
-
-            // Get the Uri of data
-            filePath = data.getData();
-            try {
-
-                // Setting image on image view using Bitmap
-                Bitmap bitmap = MediaStore
-                        .Images
-                        .Media
-                        .getBitmap(
-                                getContentResolver(),
-                                filePath);
-                imageView.setImageBitmap(bitmap);
-            }
-
-            catch (IOException e) {
-                // Log the exception
-                e.printStackTrace();
-            }
-        }
-    }
-
-    // UploadImage method
-    private void uploadImage()
-    {
-        if (filePath != null) {
-
-            // Code for showing progressDialog while uploading
-            ProgressDialog progressDialog
-                    = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            // Defining the child of storageReference
-            StorageReference ref
-                    = storageReference
-                    .child(
-                            "images/"
-                                    + UUID.randomUUID().toString());
-
-            // adding listeners on upload
-            // or failure of image
-            ref.putFile(filePath)
-                    .addOnSuccessListener(
-                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
-
-                                @Override
-                                public void onSuccess(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
-
-                                    // Image uploaded successfully
-                                    // Dismiss dialog
-                                    progressDialog.dismiss();
-                                    Toast
-                                            .makeText(MainActivity.this,
-                                                    "Image Uploaded!!",
-                                                    Toast.LENGTH_SHORT)
-                                            .show();
-                                }
-                            })
-
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e)
-                        {
-
-                            // Error, Image not uploaded
-                            progressDialog.dismiss();
-                            Toast
-                                    .makeText(MainActivity.this,
-                                            "Failed " + e.getMessage(),
-                                            Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-                    })
-                    .addOnProgressListener(
-                            new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-                                // Progress Listener for loading
-                                // percentage on the dialog box
-                                @Override
-                                public void onProgress(
-                                        UploadTask.TaskSnapshot taskSnapshot)
-                                {
-                                    double progress
-                                            = (100.0
-                                            * taskSnapshot.getBytesTransferred()
-                                            / taskSnapshot.getTotalByteCount());
-                                    progressDialog.setMessage(
-                                            "Uploaded "
-                                                    + (int)progress + "%");
-                                }
-                            });
-        }
-    }
 }
